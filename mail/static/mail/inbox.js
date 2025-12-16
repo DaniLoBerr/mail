5aae6213a02915b67f91032bf1a68f4fa88de86e
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-12-16 13:52:24 +0000

    feat(mailbox): Display multiple mailboxes
    - Update inbox.js to fetch and display a list of emails from Inbox,
    Sent, and Archive.
    - Sort emails by date in descending order (more recent first).

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -45,11 +45,58 @@ function display_message(message) {
 function load_mailbox(mailbox) {
 
 	// Show the mailbox and hide other views
-	document.querySelector('#emails-view').style.display = 'block';
+	const emails_view = document.querySelector('#emails-view');
+	emails_view.style.display = 'block';
 	document.querySelector('#compose-view').style.display = 'none';
 
 	// Show the mailbox name
 	document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+	// Get emails from API
+	fetch(`/emails/${mailbox}`)
+		.then(response => response.json())
+		.then(emails => {
+
+			// Sort emails from most recent to least recent
+			emails.sort((a, b) => b.timestamp - a.timestamp);
+
+			// Display emails
+			emails.forEach(email => {
+
+				// Separate each email in its own box
+				email_container = document.createElement('div');
+				email_container.className = "list-group list-group-horizontal";
+
+				// Divide each email in three sections
+				sender = document.createElement('div');
+				subject = document.createElement('div');
+				date = document.createElement('div');
+
+				// Change email's background-color is it is read or unread
+				if (email.read) {
+					sender.className = "list-group-item col-3 fw-bold list-group-item-dark";
+					subject.className = "list-group-item col-6 text-truncate list-group-item-dark";
+					date.className = "list-group-item col-3 fw-light list-group-item-dark";
+					
+				} else {
+					sender.className = "list-group-item col-3 fw-bold";
+					subject.className = "list-group-item col-6 text-truncate";
+					date.className = "list-group-item col-3 fw-light";
+				}
+
+				// Populate each section
+				sender.textContent = email.sender;
+				subject.textContent = email.subject;
+				date.textContent = email.timestamp;
+
+				// Display email
+				email_container.appendChild(sender);
+				email_container.appendChild(subject);
+				email_container.appendChild(date);
+
+				emails_view.appendChild(email_container);
+			});
+		});
 }
 
 function send_email(recipients, subject, body) {
